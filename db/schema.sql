07ce7609b46ca801aae940b0593592415f17ec34
itsrayito <rayito.aguirre94@gmail.com>
2023-05-15 23:37:48 -0500

    fixing bugs

--- db/schema.sql
+++ db/schema.sql
@@ -4,12 +4,12 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL
     salary DECIMAL,
     department_id INT,
@@ -20,14 +20,14 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     job_title varchar (30),
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (job_title)
+    FOREIGN KEY (role_id)
     REFERENCES role(id)
     ON UPDATE CASCADE,
-    INDEX (job_title)
+    INDEX (role_id)
 );
